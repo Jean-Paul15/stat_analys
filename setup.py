c5d71d706b9f431f1f87e4cede8562c933d1dd39
Jean-Paul <adoglijeanpaul@gmail.com>
2024-12-19 21:01:40 +0000

    premier commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='stat_analysis',
+    name='stat_analys',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
